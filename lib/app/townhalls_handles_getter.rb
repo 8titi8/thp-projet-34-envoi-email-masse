9266b8d0eed795b78998d30ca47acaa21997e46d
8titi8 <raymathild@gmail.com>
2018-07-19 19:03:42 +0200

    méthode connexion twitter et recherche handles mairie OK + essai methode chercher données dans fichier JSON

--- lib/app/townhalls_handles_getter.rb
+++ lib/app/townhalls_handles_getter.rb
@@ -4,32 +4,26 @@ require 'dotenv'
 # Ceci appelle le fichier .env grâce à la gem dotenv, et enregistre toutes les données enregistrées dans une hash ENV
 Dotenv.load
 
+class Handles
 
-	def twitter_log_in
-		browser_log_in = Watir::Browser.new
-		browser_log_in.goto 'https://twitter.com/login?lang=en'
+	def search_handles(townhall_name)
+		townhall_name = "la colle sur loup"
+		browser = Watir::Browser.new
+		browser.goto 'https://twitter.com/login?lang=en'
 
-		email_user = browser_log_in.text_field(class: "js-username-field")
+		email_user = browser.text_field(class: "js-username-field")
 		# position du champ email à remplir dans la page
 		email_user.set("thp.nice@gmail.com")
 		# email de l'utilisateur
 
-		password_user = browser_log_in.text_field(class: "js-password-field")
+		password_user = browser.text_field(class: "js-password-field")
 		# position du champ password à remplir dans la page
 		password_user.set(ENV["THP_TWITTER_PASSWORD"])
 
-		submit_button = browser_log_in.button(type: "submit") 
-		# localiser le bouton
-		submit_button.click
-		# cliquer sur le bouton
-		
+		browser.send_keys(:enter)
+
 		browser.driver.manage.timeouts.implicit_wait = 3
 		#permet d'attendre que la page se charge
-	end
-
-	def search_handles
-		browser = Watir::Browser.new #(:firefox)
-		browser.goto 'https://twitter.com'
 
 		search_bar = browser.text_field(class: "search-input")
 		# position de la barre de recherche dans la page
@@ -39,14 +33,25 @@ Dotenv.load
 		search_bar.send_keys(:enter)
 		# comme si on appuyait sur entrée pour valider la recherche
 
-		browser.driver.manage.timeouts.implicit_wait = 3
+		browser.driver.manage.timeouts.implicit_wait = 20
 		#permet d'attendre que la page se charge
 
-		browser.div(class: "stream").span(class: "username").text
+		p browser.div(class: "stream").span(class: "username").text
 		# affiche le handle du 1er tweet
 
 	end
 
-twitter_log_in
+	def get_each_townhall_name
+		text = localStorage.getItem("testJSON");
+		obj = JSON.parse(text);
+		document.getElementById("demo").innerHTML = obj.name; 
+	end
+
+
+search_handles
 p "Méfait accompli, fermeture du browser"
 browser.close
+
+end
+
+Handles.new
