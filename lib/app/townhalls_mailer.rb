08e9c395ed5926b4f5237e38f5d8b76797acba7f
Gaelle <gaelle.gorgori@gmail.com>
2018-07-19 20:57:42 +0200

    petites modifs html sur fichier townhall_mailer

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -3,17 +3,23 @@
 require 'gmail'   # on va chercher la gem gmail qui va nous permettre de transmettre nos messages
 require 'dotenv'  # permet de se servir du .env qui contient le password
 require 'mail'    # permet de mettre en format html  le message envoyé
+require 'json'    # va nous permettre de récup et utiliser le fichier json
 Dotenv.load
 
 class Mailer
   attr_accessor :email, :session_gmail
-
+    # def qui va lancer gmail lorsque l'on va lancer la classe
     def initialize
-
+    # On initialise la session gmail pour la connexion et envoi des emails
+    # On déclare nos 2 variables d'instance qui vont nous servir plus tard
     @@session_gmail = Gmail.connect("thp.nice@gmail.com", ENV['PASSWORD'])
     @@email = email
     end
+    # def qui va permettre de récupérer les fichiers JSON et transformer la donner de sorte
+    # à n'utiliser que les emails
+    def address_book_emails
 
+    end
     def message
         @@session_gmail
         @@email = @@session_gmail.deliver do
@@ -22,7 +28,7 @@ class Mailer
 
         html_part do
         content_type 'text/html; charset=UTF-8'
-        body "<p>Bonjour,<br />
+        body "<font color='blue'><p>Bonjour,<br />
 
         <p>Je m'appelle Thomas, je suis élève à <em>The Hacking Project</em>, une formation au code gratuite,
         sans locaux, sans sélection, sans restriction géographique. La pédagogie de nore école est
@@ -39,7 +45,7 @@ class Mailer
 
         <p>Bien à vous,</p><br />
 
-        <p>L'équipe THP de Nice.</p>"
+        <p>L'équipe THP de Nice.</p></font>"
       end
       add_file "thp-logo.png"
         end
